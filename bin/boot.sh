f0b2e1d03121de8cc073a025cae21af16220f466
hmlingesh <Lingesh.M@happiestminds.com>
2015-01-05 11:10:58 +0530

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -26,7 +26,7 @@ erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
 # ------------------------------------------------------------------------------------------------
 cat $APP_ROOT/nginx/conf/nginx.conf
 (tail -f -n 0 $APP_ROOT/nginx/logs/*.log &)
-exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
+#exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
 #exec /tmp/staged/app/apache2/bin/httpd -k start -f /tmp/staged/app/apache2/conf/httpd.conf
 
 # ------------------------------------------------------------------------------------------------
